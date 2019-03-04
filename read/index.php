94812b2836c5fccc5a91cf4e2c7e02973d7b991a
puffyboa <julian@sankergroup.org>
2019-03-04 10:09:20 -0600

    fixed security breach in read

--- read/index.php
+++ read/index.php
@@ -1,4 +1,39 @@
-<html>
+<?php
+
+function joinPaths() {
+    $args = func_get_args();
+    $paths = array();
+    foreach ($args as $arg) {
+        $paths = array_merge($paths, (array)$arg);
+    }
+    $paths = array_map(function($p) { return trim($p, "/"); }, $paths);
+    $paths = array_filter($paths);
+    return join('/', $paths);
+}
+
+$TEXT_DIR = 'texts';
+
+$TEXTS = [];
+
+$folders = scandir($TEXT_DIR);
+foreach($folders as $folder) {
+    $folderPath = joinPaths($TEXT_DIR, $folder);
+    if (is_dir($folderPath) && substr($folder, -1) != ".") {
+        $files = scandir($folderPath);
+        foreach($files as $file) {
+            if (substr($file, -4) === ".txt") {
+                if (!$TEXTS[$folder]) {
+                    $TEXTS[$folder] = [];
+                }
+                array_push($TEXTS[$folder], $file);
+            }
+        }
+    }
+}
+
+?>
+
+<html lang="en">
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,34 +48,36 @@
 			if ($_GET["file"] == "") {
 
 				echo "<div id='library'>";
-				$folders = scandir('texts/');
-				foreach($folders as $folder) {
-					if (is_dir('texts/' . $folder) && substr($folder, -1) != ".") {
-						$files = scandir('texts/' . $folder);
-						$author = str_replace("_"," ",$folder);
-						echo sprintf("<div class='author'><h3>%s</h3>", $author);
-						foreach($files as $file) {
-							if (substr($file, -4) === ".txt") {
-								$title = ucwords(str_replace("_"," ",substr($file,0,-4)));
-								echo sprintf("<p><a href='?file=%s/%s'>%s</a></p>", $folder, $file, $title);
-							}
-						}
-						echo "</div>";
-					}
+				foreach($TEXTS as $folder=>$files) {
+                    $author = str_replace("_"," ",$folder);
+                    echo "<div class='author'><h3>$author</h3>";
+
+                    $i = 0;
+                    foreach($files as $file) {
+                        $title = ucwords(str_replace("_"," ",substr($file,0,-4)));
+                        echo "<p><a href='?file=$folder:$file'>$title</a></p>";
+                        $i++;
+                    }
+
+                    echo "</div>";
 				}
 				echo "</div>";
 
 			} else {
 
 				echo "<div id='text'>";
-				$file = sprintf("texts/%s", $_GET["file"]);
-				if (!file_exists($file)) {
-					echo "File not found.";
-				} else {
-					$fh = fopen($file, 'r');
-					$pageText = fread($fh, 1000000);
-					echo nl2br(htmlspecialchars($pageText));
-				}
+
+				$splitPath = explode(":", $_GET["file"]);
+				$folder = $splitPath[0];
+                $file = $splitPath[1];
+				if ($TEXTS[$folder] && in_array($file, $TEXTS[$folder])) {
+				    $fp = joinPaths($TEXT_DIR, $folder, $file);
+                    $fh = fopen($fp, 'r');
+                    $pageText = fread($fh, 1000000);
+                    echo nl2br(htmlspecialchars($pageText));
+                } else {
+                    echo "File not found.";
+                }
 				echo "</div>";
 
 			}
--- read/script.js
+++ read/script.js
@@ -1,17 +1,16 @@
 
 function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    const vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
 }
 
-var file = getUrlVars()["file"];
-var loc = localStorage.getItem(file);
+const file = getUrlVars()["file"];
+const loc = localStorage.getItem(file);
 window.scrollTo(0,loc);
 window.addEventListener('beforeunload', function (e) {
-	var loc = scrollY;
-	localStorage.setItem(file, loc);
+	localStorage.setItem(file, scrollY);
 });
 
